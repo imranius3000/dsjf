ffc6411d8e479199658e33e9a80e939ca5f9e540
imran <imranius3000@gmail.com>
2026-05-21 09:33:02 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import CatApp from "./components/CatApp";
+
+function App() {
+  return <CatApp />;
+}
+
+export default App;
---
+++ src/components/CatApp.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import axios from "axios";
+
+function CatApp() {
+  const [CatImage, SetCatImage] = useState("");
+
+  const FetchCatImage = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.thecatapi.com/v1/images/search",
+      );
+      SetCatImage(response.data[0].url);
+    } catch (error) {
+      console.error("Error fetching cat image:", error);
+    }
+  };
+
+  return (
+    <div>
+      <img
+        src={CatImage}
+        alt="Cat"
+        style={{ width: "300px", height: "300px" }}
+      />
+      <button onClick={FetchCatImage}>кошара блохастый</button>
+    </div>
+  );
+}
+
+export default CatApp;
